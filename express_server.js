0851764a9723378b0a24117a1417dc5fe1196f96
Lalafang33 <lalafang33@gmail.com>
2022-09-07 21:48:04 -0700

    commiting complete post login end point, logoutendpoint, updating username to userid

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookieParser = require('cookie-parser')
+const {getUserByEmail} = require('./helper')
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -11,8 +12,14 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const users = {
+};
+
+
 const generateRandomString = () => {
-  let output = Math.random().toString(36).substrint(2,8);
+  let output = Math.random().toString(36).substring(2,8);
+  return output; 
 };
 
 app.get("/", (req, res) => {
@@ -20,22 +27,25 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
+  const userId = req.cookies.user_id
   const templateVars = { 
-    username: req.cookies &&req.cookies["username"],//bandaid solution resole req.cookie error 
+    username: users[userId],
     urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
+  const userId = req.cookies.user_id
   const templateVars = { 
-    username: req.cookies["username"]
+    username: users[userId]
   }
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
+  const userId = req.cookies.user_id
   const templateVars = { 
-    username: req.cookies["username"],
+    username: users[userId],
     id: req.params.id, 
     longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
@@ -54,6 +64,21 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/registration", (req, res) => {
+  const userId = req.cookies.user_id
+  const templateVars = { 
+    username: users[userId]};
+  res.render("registration")
+});
+
+app.get("/login", (req, res) => {
+
+res.render("login")
+});
+
+
+// APP POST BELOW
 
 app.post("/urls", (req, res) => {
   let id = generateRandomString
@@ -76,19 +101,65 @@ app.post('/urls/:id/edit',(req,res)=>{
 });
 
 app.post('/login', (req, res) => {
-  console.log(req.body);
+  const email = req.body.email;
+  let userObject = getUserByEmail(email, users);
   let username = req.body.username;
-  console.log(username);
-  res.cookie("username", username);
-  res.redirect("/urls");
+  const userId = req.cookies.user_id;
+
+  if(email !== userObject){
+    const templateVars = {
+      errorMessage: "Email not registered in our Database! Please try again. "
+    }
+    res.status(403);
+    res.render("error",templateVars)
+  }
+   if(email === userObject && (password !== userObject.password)){
+    const templateVars = { errorMessage: "Incorrect password, please try again"}
+    rest.status(403);
+    res.render("error", templateVars);
+  }else {
+  users[userId] = newUser;
+  console.log(users);
+  res.cookie("user_id", userId )
+  res.redirect('/urls');
+  };
+  
 });
 
 app.post('/logout', (req, res) => {
- res.clearCookie("username");
+  const userId = req.cookies.user_id;
+ res.clearCookie("user_id", userId);
  res.redirect("/urls");
 });
 
-// app.post()
+
+app.post('/register', (req, res) => {
+  const password = req.body.password;
+  const email = req.body.email;
+  const userId = generateRandomString();
+  const newUser = {
+    userId: userId,
+    email: email,
+    password: password,
+  }; 
+
+  if(email === "" || password === "" ) {
+    const templateVars = {errorMessage: "Email or Password not valid"};
+    res.status(400);
+    res.render("error", templateVars)
+  };
+
+  if(getUserByEmail(email, users) !== null){
+    const templateVars = { errorMessage: "Email is taken, please try using another one. "};
+    res.status(400);
+    res.render("error", templateVars)
+  }
+
+  users[userId] = newUser;
+  console.log(users);
+  res.cookie("user_id", userId )
+  res.redirect('/urls');
+});
 
 
 
